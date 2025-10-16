6fe790a8adb252aa7fbdf9fb679e461ac83f00d0
aya jouney <ayajouneyofficial@gmail.com>
2025-10-17 01:52:51 +0300

    Updates

--- src/assets/export.js
+++
@@ -1,7 +0,0 @@
-import check from './check.png'
-import checked from './checked.png'
-import empty from './empty.png'
-import circle from './circle.png'
-import calendar from './calendar.png'
-import bin from './bin.png'
-export {check,checked,empty,circle,calendar,bin}
